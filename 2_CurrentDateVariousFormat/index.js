7e96dab3a93f1c14ec53448f778803da08fba516
dhirendrabam001 <dhirendrabam12345@gmail.com>
2025-07-04 05:39:25 -0700

    update 2_currentdatevariousformat

---
+++ 2_CurrentDateVariousFormat/index.js
@@ -0,0 +1,31 @@
+
+let Current = new Date();
+
+let currentDate = Current.getDate();
+let currentMonth = Current.getMonth();
+let currntYear = Current.getFullYear();
+
+if(currentDate<=31) {
+    currentDate = "0" + currentDate;
+}
+if(currentMonth<=31) {
+    currentMonth = "0" + currentMonth
+}
+console.log("mm/dd/yyyy", currentMonth + "/", currentDate + "/", currntYear);
+console.log("dd/yyy/mm", currentDate + "/", currentMonth + "/", currntYear);
+console.log("yyyy/mm/dd", currntYear + ":", currentMonth + "/", currentDate);
+console.log("yyyy/dd/mm", currntYear + "/", currentDate + "/", currentMonth);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
