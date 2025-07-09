1d2f9e35e63644366bf30b6802e0d31db165db6a
dhirendrabam001 <dhirendrabam12345@gmail.com>
2025-07-09 04:27:23 -0700

    commmit 20-exercise-foreach

---
+++ 20_Exercise_forEach/1_Exercise/index.js
@@ -0,0 +1,12 @@
+// Print greetings for each name
+// using Foreach
+// Given: ["Alice", "Bob", "Charlie"]
+
+// Task: Print "Hello, <name>" for each.
+
+const given = ["Alice", "Bob", "Charlie"];
+
+const task = given.forEach((a) => console.log("Hello" + a));
+
+// Output
+// helloAlice, hellobob, and hellocharlie
---
+++ 20_Exercise_forEach/2_Exercise/index.js
@@ -0,0 +1,24 @@
+//Sum of numbers using forEach
+
+// Given: [10, 20, 30, 40]
+
+// Task: Use forEach to compute the total (store sum in a variable).
+
+const givenArray = [10,20,30,40,50];
+
+let sum = 0;
+
+givenArray.forEach((a) => {
+    sum += a;
+})
+console.log("This is the total sum:", sum);
+
+
+
+
+
+
+
+
+
+
